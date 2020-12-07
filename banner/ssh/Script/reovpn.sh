7777a27800fe3c61f7f9f5702b58294fb0f067e0
wildyvpn <75378084+wildyvpn@users.noreply.github.com>
2020-12-07 12:21:20 +0700

    Delete reovpn.sh

--- banner/ssh/Script/reovpn.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#Created By WILDY VPN
-echo -e "Sabar Sedang Merestart OVPN............................"
-echo -e "Created By WIldyVPN...................................."
-service openvpn restart
-echo -e "Okey Selamat OVPN Berhasil Di Restart Is Jadi Sayang..."
