76f346990d2b984df0664053e06e6f2117536fae
wildyvpn <75378084+wildyvpn@users.noreply.github.com>
2020-12-07 12:23:54 +0700

    Delete resquid.sh

--- banner/ssh/Script/resquid.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Created By WILDY VPN
-neofetch
-echo -e "Sabar Sedang Merestart................................."
-echo -e "Created By WIldyVPN...................................."
-service squid3 restart
-echo -e "Okey Selamat Berhasil Di Restart Is Jadi Sayang........"
