a74beb93b0d3ab2f3f35ed69d8ac863f21c7160f
wildyvpn <75378084+wildyvpn@users.noreply.github.com>
2020-12-07 13:42:24 +0700

    Delete remin.sh

--- banner/ssh/Script/remin.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#Created By WILDY VPN
-clear
-neofetch
-echo -e "Sabar Sedang Merestart................................."
-echo -e "Created By WIldyVPN...................................."
-service webmin restart
-echo -e "Okey Selamat Berhasil Di Restart Is Jadi Sayang........"
