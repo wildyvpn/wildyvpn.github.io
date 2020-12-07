e5ca4c6391a9ea61dc3049e8af52debe68bcd7e7
wildyvpn <75378084+wildyvpn@users.noreply.github.com>
2020-12-07 12:20:56 +0700

    Delete redrop.sh

--- banner/ssh/Script/redrop.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#Created By WILDY VPN
-neofetch
-echo -e "Sabar Sedang Merestart................................."
-echo -e "Created By WIldyVPN...................................."
-service dropbear restart
-echo -e "Okey Selamat Berhasil Di Restart Is Jadi Sayang........"
